36b8541f3e1d36d1b0b442160c25b14fb1133127
Rex Soriano <awesomerex@gmail.com>
2014-09-01 02:17:47 -0400

    collision detection boom!

--- game.js
+++ game.js
@@ -28,6 +28,10 @@ var manifest = {
 
 var game = new Splat.Game(canvas, manifest);
 
+function distance(x1, y1, x2, y2){
+	return Math.sqrt(Math.pow((x1 - x2),2) + Math.pow((y1 - y2), 2));
+}
+
 function drawCircle(context, color, radius, strokeColor, strokeSize, x, y) {
 	context.beginPath();
 	context.arc(x, y, radius, 0, 2 * Math.PI, false);
@@ -44,7 +48,6 @@ function drawEntities(context, entities) {
 	});
 	for (var i in entities) {
 		if (entities.hasOwnProperty(i)) {
-			console.log("drawing");
 			entities[i].draw(context);
   		}
 	}
@@ -64,21 +67,26 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
 	var rightPaw = game.animations.get("right-paw");
 
 	scene.leftPaw = new Splat.AnimatedEntity(-300,0,rightPaw.width, rightPaw.height, rightPaw,-432,-104);
+	scene.leftPaw.radius = 100;
 	scene.leftPaw.draw = function(context){
 		Splat.AnimatedEntity.prototype.draw.call(this, context);
-		drawCircle(context, "rgba(0,0,255,.3)", 100, "rgba(0,0,255,1)", 1, this.x, this.y);
+		drawCircle(context, "rgba(0,0,255,"+this.alpha+")", this.radius, "rgba(0,0,255,1)", 1, this.x, this.y);
 	};
 	scene.rightPaw = new Splat.AnimatedEntity(500,0,leftPaw.width, leftPaw.height, leftPaw,-125,-121);
+	scene.rightPaw.radius = 100;
 	scene.rightPaw.draw = function(context){
 		Splat.AnimatedEntity.prototype.draw.call(this, context);
-		drawCircle(context, "rgba(0,0,255,.3)", 100, "rgba(0,0,255,1)", 1, this.x, this.y);
+		drawCircle(context, "rgba(0,0,255,"+this.alpha+")", this.radius, "rgba(0,0,255,1)", 1, this.x, this.y);
 	};
 
 	scene.laser = new Splat.Entity(canvas.width/2, canvas.height/2, 0, 0);
+	scene.laser.radius = 100;
 	scene.laser.draw = function(context){
-		drawCircle(context, "rgba(255,0,0,.4)", 100, "rgba(255,0,0,1)", 1, this.x, this.y);
+		drawCircle(context, "rgba(255,0,0,.4)", this.radius, "rgba(255,0,0,1)", 1, this.x, this.y);
 	};
 
+	scene.leftPaw.alpha = ".3";
+	scene.rightPaw.alpha = ".3";
 	scene.drawables = [
 	scene.leftPaw,
 	scene.rightPaw,
@@ -91,6 +99,17 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
 	this.rightPaw.x = game.mouse.x + 200;
 	this.rightPaw.y = game.mouse.y;
 
+
+	if (distance(this.laser.x, this.laser.y, this.leftPaw.x, this.leftPaw.y) < this.laser.radius + this.leftPaw.radius){
+		this.leftPaw.alpha = "1";
+	}else{
+		this.leftPaw.alpha = ".3";
+	}
+	if (distance(this.laser.x, this.laser.y, this.rightPaw.x, this.rightPaw.y) < this.laser.radius + this.rightPaw.radius){
+		this.rightPaw.alpha = "1";
+	}else{
+		this.rightPaw.alpha = ".3";
+	}
 }, function(context) {
 	// draw
 	context.fillStyle = "#FFFFFF";
