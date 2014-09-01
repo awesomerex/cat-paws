e050e36b6c9de6ec17fdca929666861f78ec2096
Rex Soriano <awesomerex@gmail.com>
2014-09-01 01:19:40 -0400

    moving cat paws with hit boxes

--- game.js
+++ game.js
@@ -5,36 +5,92 @@ var canvas = document.getElementById("canvas");
 
 var manifest = {
 	"images": {
+		"left-paw": "img/paw-left.png",
+		"right-paw": "img/paw-right.png"
 	},
 	"sounds": {
 	},
 	"fonts": {
 	},
 	"animations": {
+		"left-paw": {
+			"strip": "img/paw-left.png",
+			"frames": 1,
+			"msPerFrame": 100
+		},
+		"right-paw": {
+			"strip": "img/paw-right.png",
+			"frames": 1,
+			"msPerFrame": 100
+		}
 	}
 };
 
 var game = new Splat.Game(canvas, manifest);
 
-function centerText(context, text, offsetX, offsetY) {
-	var w = context.measureText(text).width;
-	var x = offsetX + (canvas.width / 2) - (w / 2) | 0;
-	var y = offsetY | 0;
-	context.fillText(text, x, y);
+function drawEntities(context, entities) {
+	entities.sort(function(a, b) {
+		return b.y - a.y;
+	});
+	for (var i in entities) {
+		if (entities.hasOwnProperty(i)) {
+			console.log("drawing");
+			entities[i].draw(context);
+  		}
+	}
 }
+
+// function centerText(context, text, offsetX, offsetY) {
+// 	var w = context.measureText(text).width;
+// 	var x = offsetX + (canvas.width / 2) - (w / 2) | 0;
+// 	var y = offsetY | 0;
+// 	context.fillText(text, x, y);
+// }
 
 game.scenes.add("title", new Splat.Scene(canvas, function() {
 	// initialization
+	var scene = this;
+	var leftPaw = game.animations.get("left-paw");
+	var rightPaw = game.animations.get("right-paw");
+	scene.leftPawOffsetX = -182;
+	// var leftPawOffsetY = 258;
+	// var rightPawOffsetX = 201;
+	// var rightPawOffsetY = 301;
+
+	scene.leftPaw = new Splat.AnimatedEntity(-300,0,rightPaw.width, rightPaw.height, rightPaw,0,0);
+	scene.rightPaw = new Splat.AnimatedEntity(500,0,leftPaw.width, leftPaw.height, leftPaw,0,0);
+
+	scene.leftHit = new Splat.Entity(scene.leftPaw.x, scene.leftPaw.y, 182, 258);
+	scene.rightHit = new Splat.Entity(scene.rightPaw.x, scene.rightPaw.y, 201, 301);
+
+
+
+	scene.drawables = [
+	scene.leftPaw,
+	scene.rightPaw
+	];
 }, function() {
 	// simulation
+	this.leftPaw.x = game.mouse.x - 650;
+	this.leftPaw.y = game.mouse.y;
+	this.rightPaw.x = game.mouse.x + 75;
+	this.rightPaw.y = game.mouse.y;
+
+	this.leftHit.x = this.leftPaw.x + this.leftPaw.width + this.leftPawOffsetX;
+	this.rightHit.x = this.rightPaw.x;
 }, function(context) {
 	// draw
-	context.fillStyle = "#092227";
+	context.fillStyle = "#FFFFFF";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 
-	context.fillStyle = "#fff";
-	context.font = "25px helvetica";
-	centerText(context, "Blank SplatJS Project", 0, canvas.height / 2 - 13);
+	var drawables = this.drawables.slice(0);
+	drawEntities(context, drawables);
+
+	context.strokeRect(this.leftHit.x, this.leftHit.y, this.leftHit.width, this.leftHit.height);
+	context.strokeRect(this.rightHit.x, this.rightHit.y, this.rightHit.width, this.rightHit.height);
+	context.fillStyle = "#07B4F2F";
+
+
 }));
 
 game.scenes.switchTo("loading");
